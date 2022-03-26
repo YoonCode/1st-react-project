f85d54fe109b69e235c1a73ab91aa36481d995aa
YoonCode <yjin830201@gmail.com>
2022-03-26 11:44:45 +0900

    create ExpenseDate and put date info into it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const year = props.date.toLocaleString('ko-KR', {year: 'numeric'});
-  const month = props.date.toLocaleString('ko-KR', {month: '2-digit'});
-  const day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
-
+  
   return (
   <div className="expense-item">
-    <div>{year}</div>
-    <div>{month}</div>
-    <div>{day}</div>
+    <ExpenseDate date={props.date} />
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
