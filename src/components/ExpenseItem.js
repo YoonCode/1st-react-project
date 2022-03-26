06a8ebf4b54e249bb3b9a82fe47c4d9d574b5e47
YoonCode <yjin830201@gmail.com>
2022-03-26 11:17:00 +0900

    add date info using toLocaleString

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const year = props.date.toLocaleString('ko-KR', {year: 'numeric'});
+  const month = props.date.toLocaleString('ko-KR', {month: '2-digit'});
+  const day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
+
   return (
   <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+    <div>{year}</div>
+    <div>{month}</div>
+    <div>{day}</div>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
